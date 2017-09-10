9db608a0e52ed9838fbccaa554f5024c1eba5a38
Eddie <Eddie@Edwins-MacBook-Pro.local>
2017-09-09 22:12:15 -0700

    Added a startConvo socket event

--- route/socket-route.js
+++ route/socket-route.js
@@ -10,11 +10,44 @@ module.exports = server => {
   const websocket = socketio(server);
 
   websocket.on('connection', (socket) => {
-    console.log('A client just joined on', socket.id);
+    console.log('Connection Established', socket.id);
+
 
     socket.on('message', message => {
-      console.log('MESSAGE: ', message);
-
+      debug('Message Emission');
+      /*Recieves a message object through a socket emission
+      The message is then emitted to the conversation ID.
+      The client will create a matching socket.on whenever the
+      conversations are rendered. The socket.on will update the state
+      of the individual conversation. This will broadcast
+      to all clients who's DB queries return the matching convo :D
+      */
+      new Message(message).save()
+      .then(message => {
+        socket.emit(`update-${message.convoID.toString()}`);
+      });
     });
+
+
+    socket.on('startConvo', (members, message) => {
+      debug('startConvo Emission');
+      let allProfileIDs = members.map(member => member._id);
+
+      new Conversation({members: allProfileIDs}).save()
+      .then(convo => {
+        message.convoID = convo;
+        return message.save();
+      })
+      .then(() => {
+        members.forEach(profile => {
+          socket.emit(`update-${profile.username}-convos`);
+        });
+      });
+    });
+
+    socket.on('requestBooking', (booking, user) => {
+      debug('requestBooking emission');
+    })
+
   });
 };
