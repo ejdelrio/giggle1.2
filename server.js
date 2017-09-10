d07e0763175182ec169d29b06166797f42afa519
Eddie <Eddie@Edwins-MacBook-Pro.local>
2017-09-09 20:56:32 -0700

    Added route to connect socket from client to server

--- server.js
+++ server.js
@@ -6,12 +6,17 @@ const cors = require('cors');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const http = require('http');
+const socketio = require('socket.io');
 const PORT = process.env.PORT || 3000;
 
+
 const app = express();
+const server = http.Server(app);
 const error = require('./lib/error.js');
 const userRouter = require('./route/user-route.js');
 const albumRouter = require('./route/profile/album-route.js');
+const socketRouter = require('./route/socket-route.js');
 
 dotenv.load();
 mongoose.connect(process.env.MONGODB_URI);
@@ -22,6 +27,8 @@ app.use(userRouter);
 app.use(albumRouter);
 app.use(error);
 
-app.listen(PORT, () => {
+socketRouter(server);
+
+server.listen(PORT, () => {
   debug('Server active on port: ', PORT);
 });
