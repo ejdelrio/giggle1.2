5d68bf59a51db3971a376880de8387446aa8d54e
Eddie <Eddie@Edwins-MacBook-Pro.local>
2017-09-11 14:49:45 -0700

    Fixed typo in photo router

--- route/profile/photo-route.js
+++ route/profile/photo-route.js
@@ -10,7 +10,7 @@ const jsonParser = require('body-parser').json();
 const createError = require('http-errors');
 const debug = require('debug')('giggle:track-route');
 
-const Photo = require('../../model/track.js');
+const Photo = require('../../model/photo.js');
 const bearerAuth = require('../lib/bearer-auth-middleware.js');
 const profileFetch = require('../../lib/profileFetch.js');
 
