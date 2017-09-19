5dd01ee11ae284549eb27f7152042d60d6a04579
Luis Manzo <manzosk3@gmail.com>
2017-09-19 11:58:03 -0700

    route/profile/booking-route.js

--- route/profile/booking-route.js
+++ route/profile/booking-route.js
@@ -1,1 +1,8 @@
-'use strict';
+const bookingRouter = module.exports = new Router();
+const debug = require('debug')('giggle:booking-route');
+
+bookingRouter.get('/api/booking', function(req, res, next) {
+  debug('GET /api/booking');
+  
+  
+});
